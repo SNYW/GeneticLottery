96dac20d011db915c4a814c949981e82e002ac1c
Sonny Williams <sonnywilliams@hotmail.co.uk>
2019-09-18 21:00:07 +0100

    Added Enemy Tracking

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+
+    public List<Enemy> allTargets = new List<Enemy>();
+    public Enemy target;
+
+    public float speed;
+    public float attackRange;
+    public bool canMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canMove = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(hasTarget() && inRange())
+        {
+            attack();
+        }
+
+
+       if(hasTarget() && canMove && !inRange())
+        {
+            
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void attack()
+    {
+        print("HIYAAA");
+        allTargets.Remove(target);
+        Destroy(target, 2f);
+        
+    }
+
+    public bool hasTarget()
+    {
+        return allTargets.Count > 0;
+    }
+
+    public bool inRange()
+    {
+        if (hasTarget())
+        {
+            return Vector3.Distance(transform.position, allTargets[0].transform.position) < attackRange;
+            
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void findTarget()
+    {
+        allTargets.Clear();
+        
+        target = null;
+        float closest = 100000f;
+        Enemy tempTarget = null;
+        foreach (Enemy e in allTargets)
+        {
+            float distance = Vector3.Distance(transform.position, e.transform.position);
+            if (distance < closest)
+            {
+                tempTarget = e;
+                closest = distance;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    public float speed;
+    public float endX;
+    public float startX;
+    private GameManager gm;
+
+    private void Awake()
+    {
+        gm = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gm.moving)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        if (transform.position.x <= endX)
+        {
+            Vector2 pos = new Vector2(startX, transform.position.y);
+            transform.position = pos;
+        }
+
+    }
+}
